0ea7efde13fc5721ef57b2c6975c800155a661a9
Craig <boardies@msn.com>
2016-07-14 00:10:55 +1200

    Enable the screeps push task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,40 +8,44 @@
 
 'use strict';
 
+const privateOptions = require(`${__dirname}/.private`);
+
 module.exports = function (grunt) {
-
     // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                'src/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        'src/*.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
 
-        // Configuration to be run (and then tested).
-        scrups: {
-            options: {
-                email: 'email@gmail.com',
-                password: '*****',
-                branch: 'default'
-            },
-            pull: {
-              dest: 'src'
-            }
-        }
-    });
+    // Configuration to be run (and then tested).
+    screeps: {
+      options: privateOptions,
+      dist: {
+        src: ['src/*.js'],
+      },
+    },
+
+    scrups: {
+      options: privateOptions,
+      pull: {
+        dest: 'src',
+      },
+    },
+  });
 
     // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-git');
-    grunt.loadNpmTasks('grunt-screeps');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-git');
+  grunt.loadNpmTasks('grunt-screeps');
 };
--- src/soldier.js
+++ src/soldier.js
@@ -9,28 +9,28 @@
 
 var Soldier = {
 
-    ROLE: 'soldier',
-    
+  ROLE: 'soldier',
+
     /**
      * Operations a soldier can perform
      */
-    OPERATION: {
-      WAITING: 'waiting',
-      PATROLING: 'patroling',
-      DEFENDING: 'defending',
-      ATTACKING: 'attacking'
-    },
-    
+  OPERATION: {
+    WAITING: 'waiting',
+    PATROLING: 'patroling',
+    DEFENDING: 'defending',
+    ATTACKING: 'attacking',
+  },
+
     /**
      * The cost of soldier parts
      */
-    COST: {
-        MOVE: 50,
-        ATTACK: 80,
-        RANGED_ATTACK: 150,
-        TOUGH: 10
-    },
-    
+  COST: {
+    MOVE: 50,
+    ATTACK: 80,
+    RANGED_ATTACK: 150,
+    TOUGH: 10,
+  },
+
     /**
      * Create a new soldier
      * @param spawner the spawner to create the soldier from
@@ -38,71 +38,70 @@ var Soldier = {
      * @param maxEnergy the max energy allowed to create the soldier
      * @return the new soldier
      */
-    create: function(spawner, ranged, maxEnergy = -1) {
-        var e = spawner.room.energyAvailable;
-        if (maxEnergy >= 0 && maxEnergy < e) {
-            e = maxEnergy;
-        }
-        
-        var attackCost = ranged? Soldier.COST.RANGED_ATTACK : Soldier.COST.ATTACK;
-        var attackPart = ranged? RANGED_ATTACK : ATTACK;
-        var body = [MOVE, TOUGH, attackPart];
-        
-        e -= attackCost - Soldier.COST.MOVE - Soldier.COST.TOUGH;
-        if (e < 0) {
-            return null;
-        }
-        
-        var minCost = Soldier.COST.TOUGH;
-        while (e >= minCost) {
-            if (e >= attackCost) {
-                body.push(attackPart);
-                e = e - attackCost;
-                console.log("added " + attackPart + " - energy=" + e);
-            }
-            if (e >= Soldier.COST.MOVE) {
-                body.push(MOVE);
-                e = e - Soldier.COST.MOVE;
-                console.log("added move - energy=" + e);
-            }
-            if (e >= Soldier.COST.TOUGH) {
-                body.push(TOUGH);
-                e = e - Soldier.COST.TOUGH;
-                console.log("added toughness - energy=" + e);
-            }
-        }
-        
-        var soldier = spawner.createCreep(body, null, { role: Soldier.ROLE, operation: Soldier.OPERATION.WAITING });
-        if (!_.isString(soldier)) {
-            console.log("Failed to spawn new soldier with body " + body);
-            return null;
-        }
-        
-        console.log("Spawned soldier-" + soldier + " with body " + body);
-        return Game.creeps[soldier];
-    },
-    
+  create(spawner, ranged, maxEnergy = -1) {
+    var e = spawner.room.energyAvailable;
+    if (maxEnergy >= 0 && maxEnergy < e) {
+      e = maxEnergy;
+    }
+
+    var attackCost = ranged ? Soldier.COST.RANGED_ATTACK : Soldier.COST.ATTACK;
+    var attackPart = ranged ? RANGED_ATTACK : ATTACK;
+    var body = [MOVE, TOUGH, attackPart];
+
+    e -= attackCost - Soldier.COST.MOVE - Soldier.COST.TOUGH;
+    if (e < 0) {
+      return null;
+    }
+
+    var minCost = Soldier.COST.TOUGH;
+    while (e >= minCost) {
+      if (e >= attackCost) {
+        body.push(attackPart);
+        e = e - attackCost;
+        console.log('added ' + attackPart + ' - energy=' + e);
+      }
+      if (e >= Soldier.COST.MOVE) {
+        body.push(MOVE);
+        e = e - Soldier.COST.MOVE;
+        console.log('added move - energy=' + e);
+      }
+      if (e >= Soldier.COST.TOUGH) {
+        body.push(TOUGH);
+        e = e - Soldier.COST.TOUGH;
+        console.log('added toughness - energy=' + e);
+      }
+    }
+
+    var soldier = spawner.createCreep(body, null, { role: Soldier.ROLE, operation: Soldier.OPERATION.WAITING });
+    if (!_.isString(soldier)) {
+      console.log('Failed to spawn new soldier with body ' + body);
+      return null;
+    }
+
+    console.log('Spawned soldier-' + soldier + ' with body ' + body);
+    return Game.creeps[soldier];
+  },
+
     /**
      * Perform a soldier action
      * @param soldier the soldier
      * @return the soldier for chaining
      */
-    work: function(soldier) {
-        
-        operation = soldier.memory.operation;
-        if (operation === null) {
-            operation = Soldier.OPERATION.WAITING;
-        }
-        switch (operation) {
-            case Soldier.OPERATION.WAITING: break;
-            case Soldier.OPERATION.PATROLING: break;
-            case Soldier.OPERATION.DEFENDING: break;
-            case Soldier.OPERATION.ATTACKING: break;
-            default: break;
-        }
-        
-        return soldier;
+  work(soldier) {
+    operation = soldier.memory.operation;
+    if (operation === null) {
+      operation = Soldier.OPERATION.WAITING;
     }
+    switch (operation) {
+      case Soldier.OPERATION.WAITING: break;
+      case Soldier.OPERATION.PATROLING: break;
+      case Soldier.OPERATION.DEFENDING: break;
+      case Soldier.OPERATION.ATTACKING: break;
+      default: break;
+    }
+
+    return soldier;
+  },
 };
 
 module.exports = Soldier;
