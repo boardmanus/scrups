6676b2e885007f66e2c0f4e8550ebada2268c8d7
Craig Boardman <craig_boardman@trimble.com>
2016-07-14 14:15:53 +1200

    Fixed eslint issues

--- src/main.js
+++ src/main.js
@@ -1,125 +1,125 @@
-var SourceInfo = require('info.source');
-var RoomInfo = require('info.room');
-var Worker = require('worker');
-var Repairer = require('worker.repairer');
-var Storer = require('worker.storer');
-var Upgrader = require('worker.upgrader');
-var Waiter = require('worker.waiter');
-var Harvester = require('worker.harvester');
-var Builder = require('worker.builder');
-var Spawner = require('spawner');
-var Dismantler = require('worker.dismantler');
+const Worker = require('worker');
+const Repairer = require('worker.repairer');
+const Storer = require('worker.storer');
+const Upgrader = require('worker.upgrader');
+const Waiter = require('worker.waiter');
+const Harvester = require('worker.harvester');
+const Builder = require('worker.builder');
+const Dismantler = require('worker.dismantler');
 
-function debug(obj, msg) {
-    const OUTPUT_DEBUG = false;
-    if (!OUTPUT_DEBUG) {
-      return;
-    }
-
-    var name = "";
-    if (obj instanceof Structure) {
-        name = obj.structureType + "-" + obj.id + ": ";
-    }
-    else if (obj instanceof Creep) {
-        name = obj.memory.role + "-" + obj.name + "-" + obj.memory.operation + ": ";
-    }
-
-    console.log(name + msg);
+/**
+ * @return an objects name for output
+ */
+function oname(obj) {
+  if (obj instanceof Structure) {
+    return `${obj.structureType}'-'${obj.id}`;
+  } else if (obj instanceof Creep) {
+    return ((obj.my) ?
+      `${obj.memory.role}'-'${obj.name}` :
+      `enemy-${obj.owner}-${obj.name}`);
+  }
+  return `unknown-${obj}`;
 }
 
-module.exports.loop = function () {
 
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
-        }
+/**
+  * Mainloop of the screeps application
+  */
+module.exports.loop = function mainLoop() {
+  for (let i = 0; i < Memory.creeps.length; ++i) {
+    if (!Game.creeps[i]) {
+      delete Memory.creeps[name];
+      console.log('Clearing non-existing creep memory:', name);
+    }
+  }
+
+  for (const [, room] of Game.rooms) {
+    // RoomInfo.init(room);
+    const workers = room.find(FIND_MY_CREEPS, (w) => w.memory.role === Worker.ROLE);
+    const spawners = room.find(FIND_MY_SPAWNS);
+    const upgraders = workers.filter((w) => w.memory.operation === Upgrader.OPERATION);
+    const builders = workers.filter((w) => w.memory.operation === Builder.OPERATION);
+    const repairers = workers.filter((w) => w.memory.operation === Repairer.OPERATION);
+    const harvesters = workers.filter((w) =>
+      w.memory.operation === Harvester.OPERATION
+    );
+    const storers = workers.filter((w) => w.memory.operation === Storer.OPERATION);
+    const waiters = workers.filter((w) => w.memory.operation === Waiter.OPERATION);
+    const dismantlers = workers.filter((w) =>
+      w.memory.operation === Dismantler.OPERATION
+    );
+    const towers = room.find(FIND_MY_STRUCTURES, (s) =>
+      s.structureType === STRUCTURE_TOWER
+    );
+    const enemies = room.find(FIND_HOSTILE_CREEPS);
+    const sources = room.find(FIND_SOURCES);
+    // const flags = room.find(FIND_FLAGS);
+
+    let highways = [];
+    if (room.memory.highways && spawners.length === 0) {
+      sources.forEach((source) => {
+        highways.push(room.controller.pos.findPathTo(source));
+        spawners.forEach((spawner) => {
+          highways.push(spawner.pos.findPathTo(source));
+        });
+      });
+
+      console.log(`Surveyed ${highways.length} highways.`);
+      room.memory.highways = highways;
+
+      highways.forEach((highway) => {
+        highway.forEach((pos) => {
+          const res = room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
+          if (res !== 0) {
+            console.log(`Failed to construct road at ${pos} (err=${res})`);
+          }
+        });
+      });
+    } else {
+      highways = room.memory.highways;
     }
 
-    for (var roomName in Game.rooms) {
-
-        var room = Game.rooms[roomName];
-        //RoomInfo.init(room);
-        var workers = room.find(FIND_MY_CREEPS, (creep) => creep.memory.role == Worker.ROLE);
-        var spawners = room.find(FIND_MY_SPAWNS);
-        var upgraders = _.filter(workers, function(w) { return w.memory.operation === Upgrader.OPERATION; });
-        var builders = _.filter(workers, function(w) { return w.memory.operation === Builder.OPERATION; });
-        var repairers = _.filter(workers, function(w) { return w.memory.operation === Repairer.OPERATION; });
-        var harvesters = _.filter(workers, function(w) { return w.memory.operation === Harvester.OPERATION; });
-        var storers = _.filter(workers, function(w) { return w.memory.operation === Storer.OPERATION; });
-        var waiters = _.filter(workers, function(w) { return w.memory.operation === Waiter.OPERATION; });
-        var dismantlers = _.filter(workers, function(w) { return w.memory.operation === Dismantler.OPERATION; });
-        var towers = room.find(FIND_MY_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_TOWER })
-        var enemies = room.find(FIND_HOSTILE_CREEPS);
-        var sources = room.find(FIND_SOURCES);
-        var flags = room.find(FIND_FLAGS);
-
-        var highways = [];
-        if (room.memory.highways == null && spawners.length == 0) {
-            sources.forEach(function(source) {
-                highways.push(room.controller.pos.findPathTo(source));
-                spawners.forEach(function(spawner) {
-                   highways.push(spawner.pos.findPathTo(source));
-                });
-            });
-
-            console.log("Surveyed " + highways.length + " highways.");
-            room.memory.highways = highways;
-
-            highways.forEach(function(highway) {
-                highway.forEach(function(pos) {
-                    var res = room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
-                    if (res != 0) {
-                       console.log("Failed to construct road at " + pos + " (err=" + res + ")");
-                    }
-                });
-            });
+    spawners.forEach((spawner) => {
+      if (workers.length < 10 && !spawner.spawning) {
+        const w = Worker.create(spawner, { minEnergy: 3 * room.energyAvailable / 4 });
+        if (w) {
+          console.log(`Adding new worker ${oname(w)}`);
         }
-        else {
-            highways = room.memory.highways;
+      }
+    });
+
+
+    console.log(`Workers: ${upgraders.length} upgraders, ${builders.length} builders, ${repairers.length} repairers, ${harvesters.length} harvesters, ${storers.length} storers, ${dismantlers.length} dismantlers and ${waiters.length} waiters.`);
+    towers.forEach((t) => {
+      if (t.energy === 0) {
+        return;
+      }
+      if (enemies.length === 0) {
+        if (t.energy < t.energyCapacity / 3) {
+          return;
         }
-
-        spawners.forEach(function(spawner) {
-            if (workers.length < 10 && !spawner.spawning) {
-                var w = Worker.create(spawner, { minEnergy: 3*room.energyAvailable/4 });
-                if (w) {
-                    console.log("Adding new worker " + w.name);
-                }
-            }
+        let rs = room.find(FIND_STRUCTURES, {
+          filter: (s) =>
+            ((s.owner == null) ||
+             (s.structureType === STRUCTURE_RAMPART)) && Repairer.should_repair(s),
         });
+        rs = _.sortBy(rs, Repairer.repair_weighting);
+        console.log(`${oname(t)} has ${rs.length} repairable structures`);
+        if (rs.length > 0) {
+          const s = rs[0];
+          console.log(`${oname(t)} repairing ${oname(s)} (hits=${s.hits})`);
+          const res = t.repair(s);
+          if (res !== 0) {
+            console.log(`${oname(t)} failed to repair ${oname(s)} (${res})`);
+          }
+        }
+      } else {
+        const enemy = enemies[0];
+        console.log(`${oname(t)} attacking ${oname(enemy)}`);
+        t.attack(enemy);
+      }
+    });
 
-
-        console.log("Workers: " + upgraders.length + " upgraders, " + builders.length + " builders, " + repairers.length + " repairers, " +
-                    harvesters.length + " harvesters, " + storers.length + " storers, " + dismantlers.length + " dismantlers and " + waiters.length + " waiters.");
-        towers.forEach(function(t) {
-            if (t.energy == 0) {
-                return;
-            }
-            if (enemies.length == 0) {
-                if (t.energy < t.energyCapacity/3) {
-                    return;
-                }
-                var rs = room.find(FIND_STRUCTURES, { filter: (s) => ((s.owner == null) || (s.structureType === STRUCTURE_RAMPART)) && Repairer.should_repair(s) });
-                rs = _.sortBy(rs, Repairer.repair_weighting);
-                console.log("tower-" + t.id + " has " + rs.length + " repairable structures");
-                if (rs.length > 0) {
-                    var s = rs[0];
-                    console.log("tower-" + t.id + " repairing " + s.structureType + "-" + s.id + "(hits=" + s.hits + ")");
-                    var res = t.repair(s);
-                    if (res != 0) {
-                        console.log("tower-" + t.id + " failed to repair " + s.structureType + "-" + s.id + " (" + res + ")");
-                    }
-                }
-            }
-            else {
-                var enemy = enemies[0]
-                console.log("tower-" + t.id + " attacking enemy-" + enemy.owner + "-" + enemy.name + "-" + enemy.id);
-                t.attack(enemy);
-            }
-        });
-
-        workers.forEach(function(worker) {
-            Worker.work(worker);
-        });
-    }
-}
+    workers.forEach((worker) => Worker.work(worker));
+  }
+};
--- src/worker.dismantler.js
+++ src/worker.dismantler.js
@@ -1,126 +1,124 @@
 /*
  * Dismantler logic
  */
-var Dismantler = {
-    
-
-    OPERATION: 'dismantling',
+const Dismantler = {
 
 
-    ERROR: {
-      NONE: 0,
-      NO_SITES_TO_DISMANTLE: -1,
-      FAILED_TO_DISMANTLE: -2,
-      FULL_ENERGY: -3,
-      IS_SPAWNING: -4
-    },
-    
-    
+  OPERATION: 'dismantling',
+
+
+  ERROR: {
+    NONE: 0,
+    NO_SITES_TO_DISMANTLE: -1,
+    FAILED_TO_DISMANTLE: -2,
+    FULL_ENERGY: -3,
+    IS_SPAWNING: -4,
+  },
+
+
     /**
      * Determines if a flag is a dismantler
      * @param flag the flag to check
      * @return true if a dismantler flag
      */
-    is_dismantler_flag(f) {
-        return f.color == COLOR_BLUE;
-    },
-    
-    
+  is_dismantler_flag(f) {
+    return f.color === COLOR_BLUE;
+  },
+
+
     /**
      * Determines whether the position in the room has a dismantler flag.
      * @param room the room to check
      * @param the position in the room
      * @return true if the re are structures to be dismantled.
      */
-    has_structures_to_dismantle(room, pos) {
-        var flags = room.lookForAt(LOOK_FLAGS, pos);
-        return (_.filter(flags, Dismantler.is_dismantler_flag).length != 0);
-    },
+  has_structures_to_dismantle(room, pos) {
+    const flags = room.lookForAt(LOOK_FLAGS, pos);
+    return (_.filter(flags, Dismantler.is_dismantler_flag).length !== 0);
+  },
 
-    /**
-     * Finds  sites in the room, and orders them by priority.
-     * @param room the room to search
-     * @return an ordered array of construction sites
-     */
-    find_sites(room) {
-        var flags = room.find(FIND_FLAGS, { filter: Dismantler.is_dismantler_flag });
-        console.log("Found " + flags.length + " dismantler flags.");
-        var structures = [];
-        for (var i in flags) {
-            var flag = flags[i];
-            var flaggedStructures = room.lookForAt(LOOK_STRUCTURES, flag.pos);
-            if (flaggedStructures.length == 0) {
-                console.log("Found dismantle flag - but no structures")
-                flag.remove();
-            }
-            else {
-                console.log("Adding " + flaggedStructures.length + " structures for dismantling.");
-                structures = structures.concat(flaggedStructures);
-            }
-        }
-        
-        return structures;
-    },
-    
-    
-    /**
-     * Start/Continue the worker dismantling a site.
-     * @param worker the worker to order around
-     * @return the result of the operation
-     */
-    work: function(worker, site = null) {
-        if (worker.spawning) {
-            return Dismantler.ERROR.IS_SPAWNING;
-        }
-        
-        if (_.sum(worker.carry) > 2*worker.carryCapacity/3) {
-            console.log("worker-" + worker.name + " is carrying too much energy");
-            return Dismantler.ERROR.FULL_ENERGY;
-        }
-        
-        console.log("worker-" + worker.name + " is carrying " + _.sum(worker.carry) + "/" + worker.carryCapacity);
+  /**
+   * Finds  sites in the room, and orders them by priority.
+   * @param room the room to search
+   * @return an ordered array of construction sites
+   */
+  find_sites(room) {
+    const flags = room.find(FIND_FLAGS, { filter: Dismantler.is_dismantler_flag });
+    console.log(`Found ${flags.length} dismantler flags.`);
+    let structures = [];
+    flags.forEach((flag) => {
+      const flaggedStructures = room.lookForAt(LOOK_STRUCTURES, flag.pos);
+      if (flaggedStructures.length === 0) {
+        console.log('Found dismantle flag - but no structures');
+        flag.remove();
+      } else {
+        console.log(`Adding ${flaggedStructures.length} structures for dismantling.`);
+        structures = structures.concat(flaggedStructures);
+      }
+    });
 
-        if (!site && !worker.memory.site) {
-            var sites = Dismantler.find_sites(worker.room);
-            if (sites.length == 0) {
-                console.log("worker-" + worker.name + " found no sites to dismantle...");
-                return Dismantler.ERROR.NO_SITES_TO_DISMANTLE;
-            }
-            site = sites[worker.ticksToLive%sites.length];
-            console.log("worker-" + worker.name + " about to dismantle " + site.structureType + "-" + site.id);
-            worker.memory.site = site.id;
-        }
-        else if (!worker.memory.site) {
-            worker.memory.site = site.id;
-        }
-        else {
-            site = Game.getObjectById(worker.memory.site);
-            if (site == null) {
-                console.log("worker-" + worker.name + " site-" + worker.memory.site + " invalid!  Removing...");
-                worker.memory.site = null;
-                return Dismantler.ERROR.NO_SITES_TO_DISMANTLE;
-            }
-        }
+    return structures;
+  },
 
-        var res = worker.dismantle(site);
-        switch (res) {
-            case 0: 
-                break;
-            case ERR_NOT_IN_RANGE:
-                res = worker.moveTo(site);
-                if (res == ERR_NO_PATH) {
-                    console.log("worker-" + worker.name + " failed moving to " + site.structureType + "-" + site.id + " (" + res + ")");
-                    return Dismantler.ERROR.FAILED_TO_DISMANTLE;
-                }
-                break;
-            default: 
-                console.log("worker-" + worker.name + " failed to dismantle " + site.structureType + "-" + site.id + " (" + res + ")");
-                return Dismantler.ERROR.FAILED_TO_DISMANTLE;
-        }
-        
-        worker.memory.operation = Dismantler.OPERATION;
-        return Dismantler.ERROR.NONE;
+
+  /**
+   * Start/Continue the worker dismantling a site.
+   * @param worker the worker to order around
+   * @return the result of the operation
+   */
+  work(worker, workSite = null) {
+    let site = workSite;
+
+    if (worker.spawning) {
+      return Dismantler.ERROR.IS_SPAWNING;
     }
+
+    if (_.sum(worker.carry) > 2 * worker.carryCapacity / 3) {
+      console.log(`worker-${worker.name} is carrying too much energy`);
+      return Dismantler.ERROR.FULL_ENERGY;
+    }
+
+    console.log(`worker-${worker.name} is carrying ${_.sum(worker.carry)}/${worker.carryCapacity}`);
+
+    if (!site && !worker.memory.site) {
+      const sites = Dismantler.find_sites(worker.room);
+      if (sites.length === 0) {
+        console.log(`worker-${worker.name} found no sites to dismantle...`);
+        return Dismantler.ERROR.NO_SITES_TO_DISMANTLE;
+      }
+      site = sites[worker.ticksToLive % sites.length];
+      console.log(`worker-${worker.name} about to dismantle ${site.structureType}-${site.id}`);
+      worker.memory.site = site.id;
+    } else if (!worker.memory.site) {
+      worker.memory.site = site.id;
+    } else {
+      site = Game.getObjectById(worker.memory.site);
+      if (site == null) {
+        console.log(`worker-${worker.name} site-${worker.memory.site} invalid!  Removing...`);
+        worker.memory.site = null;
+        return Dismantler.ERROR.NO_SITES_TO_DISMANTLE;
+      }
+    }
+
+    let res = worker.dismantle(site);
+    switch (res) {
+      case 0:
+        break;
+      case ERR_NOT_IN_RANGE:
+        res = worker.moveTo(site);
+        if (res === ERR_NO_PATH) {
+          console.log(`worker-${worker.name} failed moving to ${site.structureType}-${site.id} (${res})`);
+          return Dismantler.ERROR.FAILED_TO_DISMANTLE;
+        }
+        break;
+      default:
+        console.log(`worker-${worker.name} failed to dismantle ${site.structureType}-${site.id} (${res}`);
+        return Dismantler.ERROR.FAILED_TO_DISMANTLE;
+    }
+
+    worker.memory.operation = Dismantler.OPERATION;
+    return Dismantler.ERROR.NONE;
+  },
 };
 
 module.exports = Dismantler;
