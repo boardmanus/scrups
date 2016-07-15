f42ba5afe8d781829f8c4245b8f2efbce8d0e794
Craig Boardman <craig_boardman@trimble.com>
2016-07-16 10:45:46 +1200

    Added priority for repair jobs

--- src/job.repair.js
+++ src/job.repair.js
@@ -4,6 +4,63 @@
 
 const Job = require('job');
 
+function decayPriority(job) {
+  const decay = job.site.ticksToDecay;
+  if (decay < 10) {
+    return Job.Priority.CRITICAL;
+  } else if (decay < 20) {
+    return Job.Priority.HIGH;
+  } else if (decay < 40) {
+    return Job.Priority.NORMAL;
+  } else if (decay < 80) {
+    return Job.Priority.LOW;
+  }
+
+  return Job.Priority.NEVER;
+}
+
+function defensePriority(job) {
+  const damageRatio = job.site.hits / job.site.hitsMax;
+  if (damageRatio < 0.02) {
+    return Job.Priority.CRITICAL;
+  } else if (damageRatio < 0.04) {
+    return Job.Priority.HIGH;
+  } else if (damageRatio < 0.08) {
+    return Job.Priority.NORMAL;
+  } else if (damageRatio < 0.1) {
+    return Job.Priority.LOW;
+  } else if (damageRatio < 0.9) {
+    return Job.Priority.IDLE;
+  }
+
+  return Job.Priority.NEVER;
+}
+
+function damageRatioPriority(job) {
+  const damageRatio = job.site.hits / job.site.hitsMax;
+  if (damageRatio < 0.25) {
+    return Job.Priority.HIGH;
+  } else if (damageRatio < 0.5) {
+    return Job.Priority.NORMAL;
+  } else if (damageRatio < 0.75) {
+    return Job.Priority.LOW;
+  } else if (damageRatio < 0.9) {
+    return Job.Priority.IDLE;
+  }
+  return Job.Priority.NEVER;
+}
+
+function roadPriority(job) {
+  return Math.min(decayPriority(job), damageRatioPriority(job));
+}
+
+function rampartPriority(job) {
+  return Math.min(defensePriority(job), decayPriority(job));
+}
+
+function wallPriority(job) {
+  return defensePriority(job);
+}
 
 const JobRepair = class JobRepair extends Job {
 
@@ -17,15 +74,13 @@ const JobRepair = class JobRepair extends Job {
   */
   priority() {
     switch (this.site.structureType) {
-//      case STRUCTURE_TOWER: return towerPriority(this);
-//      case STRUCTURE_LINK: return linkPriority(this);
-//      case STRUCTURE_SPAWN:
-//      case STRUCTURE_EXTENSION: return spawnerPriority(this);
-//      case STRUCTURE_CONTAINER: return containerPriority(this);
-//      case STRUCTURE_STORAGE: return storagePriority(this);
+      case STRUCTURE_ROAD: return roadPriority(this);
+      case STRUCTURE_RAMPART: return rampartPriority(this);
+      case STRUCTURE_WALL: return wallPriority(this);
       default: break;
     }
-    return Job.Priority.IDLE;
+
+    return damageRatioPriority(this);
   }
 };
 
