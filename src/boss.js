a2475522417b60a0dd9cb5304dee2e500863b876
Craig Boardman <craig_boardman@trimble.com>
2016-07-27 13:31:53 +1200

    Categorize structures in the city Start the boss creating jobs.

--- src/boss.js
+++ src/boss.js
@@ -3,33 +3,14 @@
  * creeps
  */
 const u = require('utils');
-
-
-function createJob(worker) {
-  const rememberedJob = worker.memory.job;
-  if (!rememberedJob) {
-    return null;
-  }
-
-  const site = Game.getObjectById(rememberedJob.site);
-  let job = null;
-  switch (rememberedJob.type) {
-    case JobHarvest.TYPE:
-      job = JobHarvest(site, remeberedJob.harvestRatio);
-      break;
-  }
-
-  job.assign(worker);
-}
+const Job = require('job.all');
 
 
 const Boss = class Boss {
 
   constructor(city) {
     this.city = city;
-  }
 
-  audit() {
     // Query the current crop of creeps to find active jobs.
     const activeJobs = [];
     this.city.creeps.forEach((c) => {
@@ -39,11 +20,38 @@ const Boss = class Boss {
       }
     });
 
-    let activeJobs = [];
-    const rememberedJobs = this.city.room.activeJobs;
-    if (rememberedJobs !== null) {
-      activeJobs = rememberedJobs.map((j) => createJob(j));
-    }
+    // Determine all the construction jobs to be worked
+    const constructionJobs = [];
+    city.constructionSites.forEach((cs) => {
+      const maxJobs = Job.Build.maxWorkers(cs);
+      for (let instance = 0; instance < maxJobs; ++instance) {
+        constructionJobs.push(new Job.Build(cs, instance));
+      }
+    });
+
+    const repairJobs = [];
+    city.repairableSites.forEach((s) => {
+      const maxJobs = Job.Repair.maxWorkers(s);
+      for (let instance = 0; instance < maxJobs; ++instance) {
+        repairJobs.push(new Job.Repair(s, instance));
+      }
+    });
+
+    const harvestJobs = [];
+    city.harvestSites((s) => {
+      const maxJobs = Job.Harvest.maxWorkers(s);
+      for (let instance = 0; instance < maxJobs; ++instance) {
+        harvestJobs.push(new Job.Harvest(s, instance));
+      }
+    });
+
+    const storeJobs = [];
+    city.energyStorage.forEach((s) => {
+      const maxJobs = Job.Store.maxWorkers(s);
+      for (let instance = 0; instance < maxJobs; ++instance) {
+        storeJobs.push(new Job.Store(s, instance));
+      }
+    });
   }
 
 
--- src/city.js
+++ src/city.js
@@ -4,10 +4,21 @@
 
 const u = require('./utils');
 
+
+/**
+ * Monkey patch some screeps classes...
+ */
+Structure.prototype.needsRepair = function needsRepair() {
+  return this.hits < this.hitsMax;
+};
+
+
 const City = class City {
 
   constructor(room) {
     this.room = room;
+
+    // Get all the information about the room
     this.controller = room.controller;
     this.constructionSites = room.find(FIND_MY_CONSTRUCTION_SITES);
     this.spawners = room.find(FIND_MY_SPAWNS);
@@ -16,26 +27,35 @@ const City = class City {
     this.structures = room.find(FIND_STRUCTURES);
     this.sources = room.find(FIND_SOURCES);
     this.minerals = room.find(FIND_MINERALS);
-    this.harvestableMinerals = _.filter(this.minerals, (m) =>
-      m.pos.lookFor(LOOK_STRUCTURES).length > 0
-    );
-    this.storage = this.structures.filter((s) =>
-      s.structureType === STRUCTURE_CONTAINER ||
-      s.structureType === STRUCTURE_STORAGE ||
-      (s.structureType === STRUCTURE_LINK && s.isReceiver));
+    this.harvestSites = this.minerals.filter((m) =>
+      m.pos.lookFor(LOOK_STRUCTURES).length > 0).join(this.sources);
 
     // Determine the number of various structures.
     let numRoads = 0;
     let numRamparts = 0;
     let numTowers = 0;
     let numWalls = 0;
+    let numRepairableSites = 0;
+    let numEnergyStorage = 0;
+    let numMineralStorage = 0;
     const numStructures = this.structures.length;
     for (let i = 0; i < numStructures; ++i) {
-      switch (this.structures[i].structureType) {
+      const s = this.structures[i];
+      if (s.needsRepair()) {
+        ++numRepairableSites;
+      }
+      switch (s.structureType) {
         case STRUCTURE_ROAD: ++numRoads; break;
         case STRUCTURE_RAMPART: ++numRamparts; break;
         case STRUCTURE_WALL: ++numWalls; break;
-        case STRUCTURE_TOWER: ++numTowers; break;
+        case STRUCTURE_TOWER: ++numTowers; ++numEnergyStorage; break;
+        case STRUCTURE_STORAGE: ++numEnergyStorage; ++numMineralStorage; break;
+        case STRUCTURE_CONTAINER: ++numEnergyStorage; ++numMineralStorage; break;
+        case STRUCTURE_LAB: ++numEnergyStorage; ++numMineralStorage; break;
+        case STRUCTURE_EXTENSION: ++numEnergyStorage; break;
+        case STRUCTURE_SPAWN: ++numEnergyStorage; break;
+        case STRUCTURE_LINK: ++numEnergyStorage; break;
+
         default: break;
       }
     }
@@ -45,13 +65,49 @@ const City = class City {
     this.ramparts = new Array(numRamparts);
     this.walls = new Array(numWalls);
     this.towers = new Array(numTowers);
+    this.repairableSites = new Array(numRepairableSites);
+    this.energyStorage = new Array(numEnergyStorage);
+    this.mineralStorage = new Array(numMineralStorage);
     for (let i = 0; i < numStructures; ++i) {
       const s = this.structures[i];
+      if (s.needsRepair()) {
+        this.repairableSites[--numRepairableSites] = s;
+      }
       switch (s.structureType) {
-        case STRUCTURE_ROAD: this.roads[--numRoads] = s; break;
-        case STRUCTURE_RAMPART: this.ramparts[--numRamparts] = s; break;
-        case STRUCTURE_WALL: this.walls[--numWalls] = s; break;
-        case STRUCTURE_TOWER: this.towers[--numTowers] = s; break;
+        case STRUCTURE_ROAD:
+          this.roads[--numRoads] = s;
+          break;
+        case STRUCTURE_RAMPART:
+          this.ramparts[--numRamparts] = s;
+          break;
+        case STRUCTURE_WALL:
+          this.walls[--numWalls] = s;
+          break;
+        case STRUCTURE_TOWER:
+          this.towers[--numTowers] = s;
+          this.energyStorage[--numEnergyStorage] = s;
+          break;
+        case STRUCTURE_STORAGE:
+          this.energyStorage[--numEnergyStorage] = s;
+          this.mineralStorage[--numMineralStorage] = s;
+          break;
+        case STRUCTURE_CONTAINER:
+          this.energyStorage[--numEnergyStorage] = s;
+          this.mineralStorage[--numMineralStorage] = s;
+          break;
+        case STRUCTURE_LAB:
+          this.energyStorage[--numEnergyStorage] = s;
+          this.mineralStorage[--numMineralStorage] = s;
+          break;
+        case STRUCTURE_EXTENSION:
+          this.energyStorage[--numEnergyStorage] = s;
+          break;
+        case STRUCTURE_SPAWN:
+          this.energyStorage[--numEnergyStorage] = s;
+          break;
+        case STRUCTURE_LINK:
+          this.energyStorage[--numEnergyStorage] = s;
+          break;
         default: break;
       }
     }
